9ef46cafa0c1cff3ba58ff40fba21758087f79bb
hjlopez <hjlopez@bancoagricola.com.sv>
2024-11-05 09:55:42 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar titulo="Bienvenido a la tienda"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,5 @@
+const NavBar = (props) => {
+  return <nav style={{color: 'green'}}>{props.titulo}</nav>;
+}
+
+export default NavBar;
